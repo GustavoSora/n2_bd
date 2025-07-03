76a6d938d8df927c6ab739bbc02c2199cab749e0
GustavoSora <sorahgustavo@gmail.com>
2025-07-03 20:49:50 -0300

    rota de escolher exercicios

--- app.py
+++ app.py
@@ -6,6 +6,73 @@ import json
 app = Flask(__name__)
 app.secret_key = 'chave_super_secreta'
 
+
+exercicios = [
+  {
+    "_id": "1",
+    "titulo": "Contar quantos discos de cada artista existem na coleção",
+    "resposta": "[\n{ $group: { _id: \"$artista\", totalDiscos: { $sum: 1 } } }\n]"
+  },
+  {
+    "_id": "2",
+    "titulo": "Corrigir o ano de lançamento de um álbum específico",
+    "resposta": "[\n{ $match: { titulo: \"The Dark Side of the Moon\" } },\n{ $set: { anoLancamento: 1974 } }\n]"
+  },
+  {
+    "_id": "3",
+    "titulo": "Remover o campo numeroCatalogo de todos os documentos",
+    "resposta": "[\n{ $unset: { numeroCatalogo: \"\" } }\n]"
+  },
+  {
+    "_id": "4",
+    "titulo": "Calcular a duração total (em minutos) de cada álbum",
+    "resposta": "[\n{ $project: { titulo: 1, duracaoTotalMinutos: { $divide: [ { $sum: \"$faixas.duracao_segundos\" }, 60 ] } } }\n]"
+  },
+  {
+    "_id": "5",
+    "titulo": "Adicionar um novo gênero a um álbum",
+    "resposta": "[\n{ $addToSet: { generos: \"Rock Alternativo\" } }\n]"
+  },
+  {
+    "_id": "6",
+    "titulo": "Listar todas as músicas nas quais um dos compositores é 'Roger Waters'",
+    "resposta": "[\n{ $unwind: \"$faixas\" },\n{ $match: { \"faixas.compositores\": \"Roger Waters\" } },\n{ $project: { titulo: \"$faixas.titulo\" } }\n]"
+  },
+  {
+    "_id": "7",
+    "titulo": "Listar as músicas compostas SOMENTE por 'Roger Waters', excluindo as músicas compostas juntamente com outros compositores",
+    "resposta": "[\n{ $unwind: \"$faixas\" },\n{ $match: { \"faixas.compositores\": { $eq: [\"Roger Waters\"] } } },\n{ $project: { titulo: \"$faixas.titulo\" } }\n]"
+  },
+  {
+    "_id": "8",
+    "titulo": "Alterar o nome de um dos compositores (digamos que foi digitado errado, ex.: 'Joés' ao invés de 'José')",
+    "resposta": "[\n{ $updateMany: { \"faixas.compositores\": \"Joés\" },\n{ $set: { \"faixas.$.compositores.$\": \"José\" } }\n}"
+  },
+  {
+    "_id": "9",
+    "titulo": "Encontrar a música mais longa de toda a coleção",
+    "resposta": "[\n{ $unwind: \"$faixas\" },\n{ $sort: { \"faixas.duracao_segundos\": -1 } },\n{ $limit: 1 },\n{ $project: { titulo: \"$faixas.titulo\", duracao: \"$faixas.duracao_segundos\" } }\n]"
+  },
+  {
+    "_id": "10",
+    "titulo": "Remover um disco inteiro da coleção com base no nome de um dos compositores",
+    "resposta": "[\n{ $deleteMany: { \"faixas.compositores\": \"Roger Waters\" } }\n]"
+  },
+  {
+    "_id": "11",
+    "titulo": "Calcular o número médio de faixas por disco para cada artista",
+    "resposta": "[\n{ $project: { artista: 1, numeroFaixas: { $size: \"$faixas\" } } },\n{ $group: { _id: \"$artista\", mediaFaixas: { $avg: \"$numeroFaixas\" } } }\n]"
+  },
+  {
+    "_id": "12",
+    "titulo": "Remover a última faixa do disco quando o nome do compositor for igual ao enviado por parâmetro",
+    "resposta": "[\n{ $updateMany: { \"faixas.compositores\": \"Roger Waters\" },\n{ $pop: { faixas: 1 } }\n]"
+  }
+]
+
+
+
+
 #config mongoDB
 uri = "mongodb+srv://leticia:123@clusteraula.8yd0csp.mongodb.net/?retryWrites=true&w=majority&appName=ClusterAula"
 # Create a new client and connect to the server
@@ -40,7 +107,7 @@ def mostrarExercicio(id=None):
     if request.method == 'POST':
         selected_id = request.form.get('exercicio_id')
         if selected_id:
-            return redirect(url_for('mostrarExercicio', id=selected_id))
+            return redirect(url_for('/ex', id=selected_id))
 
     # busca exercicio pelo ID
     exercicio = dataEx[id-1]
@@ -69,6 +136,16 @@ def cadastro():
         })
         return redirect(url_for('login'))
     return render_template('cadastro.html')
+
+@app.route('/escolher', methods=['GET', 'POST'])
+def escolher():
+    if request.method == 'POST':
+        selected_id = request.form.get('exercicio_id')
+        if selected_id and selected_id.isdigit():
+            return redirect(url_for('mostrarExercicio', id=int(selected_id)))
+    return render_template('selecionar_exercicio.html', exercicios=dataEx)
+
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
